e2ab1828e6b826c4d5212cea968a83fa22589605
Alisa Leung <alisal.singyee@gmail.com>
2026-06-11 20:12:28 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package prog-training-2027;
+
+public class Main{
+  public static void main (String [] args){
+      System.out.print("Hi! Write your comment in the lines below!");
+      // This is an example comment.
+  }
+}
