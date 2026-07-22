9efc6b25a64b18f111e898435eda16e03e706aeb
Alisa Leung <alisal.singyee@gmail.com>
2026-07-21 19:54:24 -0700

    generic superstructure

---
+++ unit_7/generic_superstructure/Exercise1.java
@@ -0,0 +1,6 @@
+public class Exercise1{
+  public static void main (String [] args){
+      // Create a simple table or flowchart of any subsystem and how it works with Generic Superstructure.
+      // When completed, add your finished flowchart to this folder [prog-training-2027/unit_7/generic_superstructure] as Exercise1.
+  }
+}
---
+++ unit_7/generic_superstructure/Project1.java
@@ -0,0 +1,13 @@
+public class Project1{
+  public static void main (String [] args){
+      // Now that you are a seasoned programmer, you are tasked with creating your own subsystem!
+      // Using your knowledge of Generic Superstructure and referencing other subsystems, create your own subsystem that properly utilizes Generic Superstructure.
+      // Below are some instructions so that you can get our robot's repository onto your computer!
+        // Go to https://github.com/Iron-Panthers/FRC-2026.
+        // Create a fork of the repository, and set the owner to yourself. This will create a completely separate copy of the repository for yourself!
+        // Open GitKraken, add a new tab, and clone your forked repository.
+        // In VS Code, open the repository, and find the subsystems in [src/main/java/frc/robot/subsystems].
+        // In "subsystems", create your own folder, and begin writing your own subsystem!
+      // When you're done with your subsystem, copy and paste your folder of your own subsystem into this folder [prog-training-2027/unit_7/generic_superstructure]!
+  }
+}
